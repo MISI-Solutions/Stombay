3a69dcb1696efd5dec657c35ebfc851e014daf31
BuildTools <unconfigured@null.spigotmc.org>
2020-03-29 23:16:02 +0200

    Added remaining Pop-Ups and fixed a few spelling mistakes

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,9 +1,21 @@
 const closeBtn = document.querySelectorAll(".close-button");
 const programmer = document.querySelector("#programmer");
+const uiDesigner = document.querySelector("#ui-designer");
+const builder = document.querySelector("#builder");
 const overlay = document.querySelector("#overlay");
 
 programmer.addEventListener("click", function() {
     document.querySelector("#programmer-pop-up").classList.toggle("opened");
+    overlay.classList.toggle("opened");
+});
+
+uiDesigner.addEventListener("click", function() {
+    document.querySelector("#ui-designer-pop-up").classList.toggle("opened");
+    overlay.classList.toggle("opened");
+});
+
+builder.addEventListener("click", function() {
+    document.querySelector("#builder-pop-up").classList.toggle("opened");
     overlay.classList.toggle("opened");
 });
 
